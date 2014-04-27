da0211a09a00f65142b2c1756f5f94f6d6b64fa4
Léo Baudouin <baudouin.leo@gmail.com>
2014-04-27 15:20:57 +0200

    Init files

---
+++ README.txt
@@ -0,0 +1,7 @@
+To create documentation :
+
+On linux :
+doxygen Doxyfile
+
+On windows : (to confirm)
+doxygen.exe Doxyfile
---
+++ file.m
@@ -0,0 +1,33 @@
+% ======================================================================
+%> @brief Brief description of the function 1
+%>
+%> More detailed description.
+%>
+%> @param arg1 First argument
+%> @param arg2 Second argument
+%>
+%> @retval out1 return value for the first output variable
+%> @retval out2 return value for the second output variable
+%>
+% ======================================================================
+function [out1, out2] =  function1( arg1, arg2)
+out1 = arg2;
+out2 = arg1;
+end
+
+% ======================================================================
+%> @brief Brief description of the function 2
+%>
+%> More detailed description.
+%>
+%> @param arg1 First argument
+%> @param arg2 Second argument
+%>
+%> @retval out1 return value for the first output variable
+%> @retval out2 return value for the second output variable
+%>
+% ======================================================================
+function [out1, out2] =  function2( arg1, arg2)
+out1 = arg1 + arg2;
+out2 = arg1 - arg2;
+end
